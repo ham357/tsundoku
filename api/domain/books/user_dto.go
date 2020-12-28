2bcd57f6c244ca44d8772aded3fdcefdbe021ceb
ham357 <sakuraniumarete@gmail.com>
2020-12-28 23:51:09 +0900

    Userモデルの作成

---
+++ api/domain/books/user_dto.go
@@ -0,0 +1,23 @@
+package users
+
+import (
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	"github.com/ham357/tsundoku/api/utils/errors"
+)
+
+type User struct {
+	gorm.Model
+	Name   string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (u *User) Validate() *errors.ApiErr {
+	u.Email = strings.TrimSpace(strings.ToLower(u.Email))
+	if u.Email == "" {
+		return errors.NewBadRequestError("invalid Email")
+	}
+	return nil
+}
