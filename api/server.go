b39fc90f92c0eb3a6c79c56239ec4c6529f38aba
ham357 <sakuraniumarete@gmail.com>
2021-01-06 23:52:41 +0900

    firebaseミドルウェアを登録

--- api/server.go
+++ api/server.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-	"github.com/ham357/tsundoku/api/app"
+    "github.com/ham357/tsundoku/api/app"
+    "github.com/ham357/tsundoku/api/middlewares"
     "github.com/labstack/echo"
 )
 
 func main() {
     e := echo.New()
+    e.Use(middlewares.Firebase())
     app.Init(e)
     e.Logger.Fatal(e.Start(":8080"))
 }
