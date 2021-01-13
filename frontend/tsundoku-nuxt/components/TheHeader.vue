cecda16cce9cba69dea7728831154f9ef1c6e872
ham357 <sakuraniumarete@gmail.com>
2021-01-13 22:18:10 +0900

    本投稿画面を仮実装

--- frontend/tsundoku-nuxt/components/TheHeader.vue
+++ frontend/tsundoku-nuxt/components/TheHeader.vue
@@ -10,6 +10,13 @@
       <div class="navbar-start">
         <div class="navbar-item">
           <div class="field has-addons" />
+          <div
+            v-if="isLoggedIn"
+          >
+            <nuxt-link class="navbar-item subtitle is-5" to="/books">
+              本を積む
+            </nuxt-link>
+          </div>
         </div>
       </div>
 
---
+++ frontend/tsundoku-nuxt/pages/books/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="section">
+    <div class="columns">
+      <div class="column is-6">
+        <div class="box">
+          <div class="field">
+            <label class="label">Name</label>
+            <div class="control">
+              <input
+                v-model="name"
+                class="input is-info"
+                type="text"
+                placeholder="Name"
+              >
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Detail</label>
+            <div class="control">
+              <input
+                v-model="detail"
+                class="input is-info"
+                type="text"
+                placeholder="Detail"
+              >
+            </div>
+          </div>
+
+          <div class="field">
+            <label class="label">Price</label>
+            <div class="control">
+              <input
+                v-model="price"
+                class="input is-info"
+                type="number"
+                placeholder="Price"
+              >
+            </div>
+          </div>
+
+          <div class="field is-grouped">
+            <div class="control">
+              <button
+                class="button is-link"
+                @click="login"
+              >
+                Submit
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        login() {
+            const user = {
+                email: this.email,
+                password: this.password
+            };
+            this.$store.dispatch('login', user);
+        }
+    }
+};
+</script>
